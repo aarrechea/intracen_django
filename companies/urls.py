f219de87bfbff2654d38ae775066a4b08f8d2d07
aarrechea <adrianarrechea@yahoo.com.ar>
2023-03-31 21:51:52 -0300

    responsive menu

--- companies/urls.py
+++ companies/urls.py
@@ -19,8 +19,9 @@ companies_patterns = ([
 ], 'companies')
 
 
-"""companies_patterns = ([
-    path('companies/', views.companies_list, name='companies'),
-    path('companies/add/<str:action>/', views.add_company, name='add'),
-    path('companies/add/<str:action>/<int:id_company>/', views.add_company, name='add'),
-], 'companies')"""
+
+
+
+
+
+
